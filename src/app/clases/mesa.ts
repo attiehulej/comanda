db6591c61786808ec3693581fef7879a3d65be31
Franco Lippi <soyflippi@gmail.com>
2020-06-05 16:29:48 -0300

    Se modifica clases Mesa, Producto

--- src/app/clases/mesa.ts
+++ src/app/clases/mesa.ts
@@ -1,2 +1,15 @@
-export default class Mesa {
+export enum Tipo {
+    VIP = 'VIP',
+    DISCAPACITADO = 'DISCAPACITADO'
+}
+
+export class Mesa {
+    public id?: string;
+    public numero?: string;
+    public cantidad?: string;
+    public foto?: string;
+    public tipo: Tipo;
+    public fechaAlta: Date = new Date();
+    public fechaModificado: Date = null;
+    public fechaBaja: Date = null;
 }
