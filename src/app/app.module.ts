59bac1b931209b8a3cb819c21b45f98c93717463
patoattie <patoattie@gmail.com>
2020-06-14 01:21:15 -0300

    incluyo ReactiveFormsModule en el modulo principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,7 @@ import { Camera } from '@ionic-native/camera/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CommonModule } from '@angular/common';
-//import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AngularFireModule } from '@angular/fire'; // PATO
 // import { AngularFireModule } from 'angularfire2'; // AGREGO LUCAS
